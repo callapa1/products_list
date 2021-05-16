82ff6f2c96ec1280f5a622970e7a22bffa20afc5
Cristian <54779545+callapa1@users.noreply.github.com>
2021-05-16 15:08:16 +0200

    Indentation fixed

--- public_html/helpers/validation_helper.php
+++ public_html/helpers/validation_helper.php
@@ -1,9 +1,9 @@
 <?php
 
 Class ValidationHelper {
-  static function checkIfEmpty($array){
-      foreach ($array as $item){
-          return ($item !== NULL && $item !== FALSE && $item !== "");
-      }
-  }
+    static function checkIfEmpty($array){
+        foreach ($array as $item){
+            return ($item !== NULL && $item !== FALSE && $item !== "");
+        }
+    }
 }
